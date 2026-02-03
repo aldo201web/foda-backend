bead65055ef1e35f63c5dc67e8ff2ef19b89f515
aldo201web <aldogamer8@gmail.com>
2026-02-03 16:33:26 -0600

    CORS

--- src/main/java/mx/uv/foda/foda_backend/controller/PingController.java
+++ src/main/java/mx/uv/foda/foda_backend/controller/PingController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingController {
-
+//Levantado o no
     @GetMapping("/api/ping")
     public String ping() {
         return "Conexion a MySQL OK";
